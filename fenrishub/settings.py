485d40d1d944617e61e6fa862e05c9b116c774cd
struppigel <struppigel@googlemail.com>
2026-03-22 12:08:45 +0100

    fix

--- fenrishub/settings.py
+++ fenrishub/settings.py
@@ -119,8 +119,7 @@ if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    SECURE_SSL_REDIRECT = _env_bool('SECURE_SSL_REDIRECT', True)
-    STORAGES = {
+STORAGES = {
         'default': {
             'BACKEND': 'django.core.files.storage.FileSystemStorage',
         },
